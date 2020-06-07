20633a3d2b53c67e699af50bb4fe6d1183c84e88
Nishidh <shekhawatnishidh5@gmail.com>
2020-06-07 23:08:08 +0530

    First Commit

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+ // Configure your networks
+  networks: {
+
+   // development network is default for truffle
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "5777",       // Any network (default: none)
+    }
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+}
